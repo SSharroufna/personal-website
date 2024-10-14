16fa38ac5f5e04312fc5cf25a850c2f5e3593c17
SarahSharroufna <sash1048@colorado.edu>
2024-10-13 18:48:49 -0600

    Add letter S svg

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 import './style.css'
 import { gsap } from 'gsap';
 
-// Select the element to animate
-const myElement = document.getElementById('myElement');
-
-// Animate the element
-gsap.to(myElement, { rotation: 360, duration: 2 });
-
